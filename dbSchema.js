377de2a9bb9966adf13dc58bf5e4e5c937ad2a71
CornetS28 <corsam28@gmail.com>
2020-10-01 17:25:25 -0400

    add dbSchema.js file

---
+++ dbSchema.js
@@ -0,0 +1,29 @@
+let db = {
+  users: [
+    {
+      userId: "dh23ggj5h32g543j5gf43",
+      firstName: "Daniel",
+      lastName: "Doe",
+      phone: "+50937033033",
+      email: "user@email.com",
+      handle: "daniel",
+      createdAt: "2020-10-15T10:39:32.798Z",
+      imageUrl: "image/dsfsaaaasdss/assdlllfgdh",
+      bio: "Hello, my name is daniel, I am a doctor",
+      website: "https://daniel.com",
+      location: "NY, US",
+    },
+  ],
+  departments: [
+    {
+      departmentName: "Urology",
+      userHandle: "Samuel",
+      createAt: "2020-10-01T14:16:33.031Z",
+    },
+    {
+      departmentName: "Radiotherapy",
+      userHandle: "Samuel",
+      createAt: "2020-10-01T14:16:37.031Z",
+    },
+  ],
+};
